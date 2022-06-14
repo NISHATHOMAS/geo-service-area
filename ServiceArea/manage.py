0626972dc29ff1e6dd37dff581e85e3e3101385b
NISHA THOMAS <niz.shabzt@gmail.com>
2022-06-14 10:07:05 +0530

    Update manage.py

--- ServiceArea/manage.py
+++ ServiceArea/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servicearea.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ServiceArea.servicearea.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
