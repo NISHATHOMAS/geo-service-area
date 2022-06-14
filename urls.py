283d50dc3253df05748d3ebfea8d870b9ff5b13f
NISHA THOMAS <niz.shabzt@gmail.com>
2022-06-14 14:46:35 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import ServiceAreasListCreateApiView
-
-urlpatterns = [path("", ServiceAreasListCreateApiView.as_view(), name="ServiceAreas"),
-
-               ]
